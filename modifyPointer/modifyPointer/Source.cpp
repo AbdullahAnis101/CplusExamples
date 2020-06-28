3f7b76b1d5cab86f30f151a23cb2a7de084687d7
Abdullah Anis <abdullahanis12@gmail.com>
2020-06-28 11:58:55 -0500

    Add files via upload

---
+++ modifyPointer/modifyPointer/Source.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	string food = "Pizza";
+	string* ptr = &food;
+
+
+	//Output the value of food (Pizza)
+
+	cout << food << "\n";
+
+	// Output the memory address of food
+
+	cout << &food << "\n";
+
+	// Access the memory address of food and output its value
+
+	cout << *ptr << "\n";
+
+	// Change the value of the pointer
+
+	*ptr = "Hamburger";
+
+	// Output the new value of the pointer 
+	cout << *ptr << "\n";
+
+	//Output the new value of the food variable (Hamburger)
+	cout << food << "\n";
+
+	// Output the memory address of food. (Doesn't change the memory address)
+	cout << &food << "\n";
+
+
+
+}
