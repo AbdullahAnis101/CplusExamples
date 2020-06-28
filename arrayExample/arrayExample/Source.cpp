b0af6a115b4f0ca48d827b2a659c806b0d6d9e4e
Abdullah Anis <abdullahanis12@gmail.com>
2020-06-28 11:42:16 -0500

    Add files via upload

---
+++ arrayExample/arrayExample/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	string cars[4] = {"Volvo", "BMW", "Ford" , "Mazda"};
+	cars[0] = "OPal";
+	cout << cars[0];
+
+
+
+
+
+}
